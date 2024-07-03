beaee937df2ba87f22dd449c8695084a9399d781
khobragade2001 <khobragade2001@gmail.com>
2024-07-03 15:01:10 +0530

    emplicit wait and new file "fluent_wait " is created

--- Files/Implicit_wait.py
+++ Files/Implicit_wait.py
@@ -1,10 +1,21 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.alert import Alert
 
-d = webdriver.Chrome()
+driver = webdriver.Chrome()
 x = By.XPATH
-d.implicitly_wait(15)
-d.get("https://fast.com/")
+driver.implicitly_wait(15)
+alert = Alert(driver)
+driver.get("https://seleniumpractise.blogspot.com/")
+
+## click on alert element
+driver.find_element(x, "//button[@onclick='myFunction()']").click()
+time.sleep(11)
+alert.accept()
+
+##======================= explicit wait ====================
 
 
 
